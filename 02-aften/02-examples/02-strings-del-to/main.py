bc44d5ce47f9820e1735608081a90cfee243db6b
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-08-23 22:40:17 +0200

    Added example

---
+++ 02-aften/02-examples/02-strings-del-to/main.py
@@ -0,0 +1,16 @@
+# 'upper' metoden
+text = "Hej Mogens!"
+print(text.upper())
+
+# 'strip' metoden
+many_whitespaces = "     Hey hummersuppe!      "
+print(many_whitespaces.strip())
+
+# Sammenkædning af strings
+first_name = "Jonas"
+last_name = "Phillipson"
+full_name = first_name + " " + last_name
+print(full_name)
+
+# 'f-strings' metoden
+print(f"hej, mit navn er {full_name}")
