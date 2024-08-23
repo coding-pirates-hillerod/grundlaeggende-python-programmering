ec8f9a3a87e49d97bfe84d77f7b93daa361d9895
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-08-23 22:32:59 +0200

    Added exercise

---
+++ 02-aften/03-exercises/02-exercise/main.py
@@ -0,0 +1,5 @@
+"""
+Lav en "name" variabel og sæt den lig med dit fulde navn.
+
+Brug "bracket notation" til kun at print fornavnet af dit fulde navn ud i terminalen.
+"""
---
+++ 02-aften/03-exercises/02-exercise/solution/main.py
@@ -0,0 +1,2 @@
+full_name = "Jonas Bak Phillipson"
+print(full_name[0:5])
