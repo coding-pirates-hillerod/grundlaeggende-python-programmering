8168da3eaab892709b1fa30e331f19bd6a1b37d5
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-08-24 14:49:11 +0200

    Added example and exercise

---
+++ 02-aften/02-examples/06-sammenligningsoperatorer/main.py
@@ -0,0 +1,17 @@
+num1 = 10
+num2 = 20
+
+is_equal = num1 == num2
+print(is_equal)
+
+
+is_different = num1 != num2
+print(is_different)
+
+
+is_bigger = num1 > num2
+print(is_bigger)
+
+
+is_smaller = num1 < num2
+print(is_smaller)
---
+++ 02-aften/03-exercises/08-exercise/main.py
@@ -0,0 +1,11 @@
+"""
+Lav to variable kaldet "num1" og "num2". Sæt dem lig med de tal du vil.
+
+Brug følgende sammenligningsoperatorer, til at sammenligne num1 og num2:
+- ==
+- !=
+- <
+- >=
+
+Skriv resultatet af hver sammenligningsoperation ud i konsollen.
+"""
