266c33f9c725f1e3f26fd3671bb2b438d8317f13
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-08-24 10:44:37 +0200

    Added example and exercise

---
+++ 02-aften/02-examples/04-aritmetiske-operatorer/main.py
@@ -0,0 +1,30 @@
+num1 = 10
+num2 = 3
+
+
+# Addition
+result = num1 + num2
+
+
+# Subtraktion
+result = num1 - num2
+
+
+# Multiplikation
+result = num1 * num2
+
+
+# Division
+result = num1 / num2
+
+
+# Modulus
+result = num1 % num2
+
+
+# Exponentiering
+result = num1**num2
+
+
+# Floor Division
+result = num1 // num2
---
+++ 02-aften/03-exercises/06-exercise/main.py
@@ -0,0 +1,5 @@
+"""
+Lav to variable kaldet "num1" og "num2" og sæt dem lig med et tal.
+
+Print nedenunder både addition, subtraktion, multiplikation, division, modulus og floor division af tallene.
+"""
