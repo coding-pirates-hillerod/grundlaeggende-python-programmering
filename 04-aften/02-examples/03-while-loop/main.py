fb234450bda5083da064a2623da2b1010142c293
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-08-25 09:15:38 +0200

    Added example and exercise

---
+++ 04-aften/02-examples/03-while-loop/main.py
@@ -0,0 +1,5 @@
+# et 'while loop' som tæller fra 1 til 10
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
---
+++ 04-aften/03-exercises/03-exercise/main.py
@@ -0,0 +1,5 @@
+"""
+Laav først en variabel kaldet "number" og sæt den lig med tallet 10.
+
+Brug derefter et while loop til at tælle ned fra 10 til 0 og printe hvert tal ud.
+"""
