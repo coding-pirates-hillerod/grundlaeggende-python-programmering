6c105ecf9003b21dd7e42c6d850b73e6142876fa
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-08-25 09:50:32 +0200

    Added exercise

---
+++ 04-aften/03-exercises/05-exercise/main.py
@@ -0,0 +1,8 @@
+"""
+Lav først en funktion kaldet "subtract_numbers" som tager 2 tal som argumenter - kald
+dem "num1" og "num2" - der returnerer 'num1' - 'num2'.
+
+Lav derefter en variabel kaldet "result" som sættes lig med 'subtract_numbers' med argumenterne 10 og 5.
+
+Print til sidst din "result" variabel.
+"""
---
+++ 04-aften/03-exercises/05-exercise/solution/main.py
@@ -0,0 +1,6 @@
+def subtract_numbers(num1, num2):
+    return num1 - num2
+
+
+result = subtract_numbers(10, 5)
+print(result)  # Output: 5
